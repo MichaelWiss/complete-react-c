a33c950533c36fa8706ae2989dfc396108f4ccc0
MichaelWiss <michael.wiss@gmail.com>
2023-03-17 18:09:20 -0500

    building out reducer

--- src/store/CartProvider.js
+++ src/store/CartProvider.js
@@ -7,6 +7,10 @@ const defaultCartState = {
 };
 
 const cartReducer = (state, action) => {
+  if (action.type === 'ADD') {
+     const updatedItems = state.items.concat(action.item);
+     const updatedTotalAmount = state.totalAmount + action.item.price * action.item.amount;
+  }
   return defaultCartState;
 };
 
@@ -15,10 +19,12 @@ const CartProvider = props => {
   const [cartState, dispatchCartAction] = useReducer(cartReducer, defaultCartState);
 
     const addItemToCartHandler = (item )=> {
-      dispatchCartAction({type: 'ADD'});
+      dispatchCartAction({type: 'ADD', item: item});
     };
 
-    const removeItemFromCartHandler = (id) => {};
+    const removeItemFromCartHandler = (id) => {
+      dispatchCartAction({type: 'REMOVE', id: id});
+    };
 
 
     const cartContext = {
