b0964eb98d5780590d60801b05a2d74a6da45139
MichaelWiss <michael.wiss@gmail.com>
2023-03-22 16:49:01 -0500

    cart Items

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,10 +1,17 @@
+import { useContext } from 'react';
+
 import Modal from '../UI/Modal';
 import classes from './Cart.module.css';
+import CartContext from '../../store/cart-context';
 
 const Cart = props => {
+    const cartCtx = useContext(cartContext);
+
+    const totalAmount = `$${cartCtx.totalAmount.toFixed(2)}`;
+
     const cartItems = (
      <ul className={classes['cart-items']}>
-        {[{ id: 'c1', name: 'Sushi', amount: 2, price: 12.99 }].map((item) => (
+        {cartCtx.items.map((item) => (
         <li>{item.name}</li>
         ))}
         </ul>
