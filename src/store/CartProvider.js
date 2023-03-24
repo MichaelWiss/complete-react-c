e14c0b633cafed30f58f7a2fa77cffdaff92bd22
MichaelWiss <michael.wiss@gmail.com>
2023-03-23 20:00:12 -0500

    cart

--- src/store/CartProvider.js
+++ src/store/CartProvider.js
@@ -8,8 +8,11 @@ const defaultCartState = {
 
 const cartReducer = (state, action) => {
   if (action.type === 'ADD') {
-     const updatedItems = state.items.concat(action.item);
+     
      const updatedTotalAmount = state.totalAmount + action.item.price * action.item.amount;
+
+     const existingCartItemIndex = state.items.findIndex(item =>);
+     const updatedItems = state.items.concat(action.item);
      return {
       items: updatedItems,
       totalAmount: updatedTotalAmount
