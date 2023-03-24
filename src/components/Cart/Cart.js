e21991396b23712761cf94b7ec2d559bcbafc3e1
MichaelWiss <michael.wiss@gmail.com>
2023-03-23 19:12:07 -0500

    hasItems

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -28,7 +28,7 @@ const Cart = props => {
         </div>
        <div className={classes.actions}>
             <button className={classes['button--alt']} onClick={props.onClose}>Close</button>
-            hasItems && <button className={classes.button}>Order</button>
+            {hasItems && <button className={classes.button}>Order</button>}
        </div>
     </Modal>
     );
