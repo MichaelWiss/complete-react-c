7b67c982065b8a852d7dc2bd1bc79e64eb97f7a6
MichaelWiss <michael.wiss@gmail.com>
2023-04-07 18:55:32 -0500

    fetch api

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -1,12 +1,34 @@
+import { useEffect, useState } from 'react';
 import Card from '../UI/Card';
 import classes from './AvailableMeals';
 import MealItem from './MealItem/MealItem';
 
 
 const AvailableMeals = () => {
-    fetch()
+  const [meals, setMeals] = useState([]);
 
-    const mealsList = DUMMY_MEALS.map(meal => (
+    useEffect(() => {
+      const fetchMeals = async () => {
+      const response = await fetch('https://react-http-a-8e37c-default-rtdb.firebaseio.com/meals/meals.json');
+      const responseData = await response.json();
+
+      const loadedMeals = [];
+
+      for(const key in responseData) {
+        loadedMeals.push({
+          id: key,
+          name: responseData[key].name,
+          description: responseData[key].description,
+          price: responseData[key].price,
+       });
+      }
+      setMeals(loadedMeals);
+    };
+    fetchMeals();
+    }, []);
+ 
+
+    const mealsList = meals.map(meal => (
     <MealItem
         id={meal.id}
         key={meal.id} 
