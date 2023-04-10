97849f83fd1993c8f9aea65bc370c3eb1166c8a0
MichaelWiss <michael.wiss@gmail.com>
2023-04-10 16:36:46 -0500

    isLoading state

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -6,9 +6,11 @@ import MealItem from './MealItem/MealItem';
 
 const AvailableMeals = () => {
   const [meals, setMeals] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
 
     useEffect(() => {
       const fetchMeals = async () => {
+      setIsLoading(true);
       const response = await fetch('https://react-http-a-8e37c-default-rtdb.firebaseio.com/meals.json');
       const responseData = await response.json();
 
